11c029f8fe1838a8e61c35a5a1f297f460d41141
Emin Cingöz <emincingoz37@gmail.com>
2022-10-31 17:38:12 +0300

    week-4 Kodlama.io.Devs Project

---
+++ Week-4/Kodlama.io.Devs/src/main/java/com/emincingoz/KodlamaioDevs/business/requests/programmingLanguage/ProgrammingLanguageUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.emincingoz.KodlamaioDevs.business.requests.programmingLanguage;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProgrammingLanguageUpdateRequest {
+    private String name;
+}
---
+++ Week-4/Kodlama.io.Devs/src/main/java/com/emincingoz/KodlamaioDevs/entities/concretes/ProgrammingLanguage.java
@@ -0,0 +1,12 @@
+package com.emincingoz.KodlamaioDevs.entities.concretes;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProgrammingLanguage {
+    private Long id;
+    private String name;
+}
